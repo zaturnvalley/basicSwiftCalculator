f9fa2a606cd13c2d460340f40c59d1cd98a4caaa
zaturnvalley <zacharycmason@gmail.com>
2016-11-19 23:53:38 -0800

    added label, text in displ concats digits

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel?
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("touch \(digit) digit")
+        let textCurrentlyInDisplay = display!.text!
+        display!.text = textCurrentlyInDisplay + digit
     }
+    
 }
-d
+
