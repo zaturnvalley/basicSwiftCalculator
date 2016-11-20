25fb90db762c4d0b3192c21ba452056ae0b91dd6
zaturnvalley <zacharycmason@gmail.com>
2016-11-20 00:10:22 -0800

    added pi

--- ViewController.swift
+++ ViewController.swift
@@ -10,19 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
     
     var userIsInMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else {
-            display!.text = digit
+            display.text = digit
         }
         userIsInMiddleOfTyping = true
+    }
+    @IBAction func performOperation(_ sender: UIButton) {
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
     }
     
 }
