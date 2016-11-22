c923bb2e742ddd73374dd6e9157b74e5583e0d39
zaturnvalley <zacharycmason@gmail.com>
2016-11-21 19:01:54 -0800

    created calculatorBrain model

---
+++ CalculatorBrain.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Zachary Mason on 11/21/16.
+//  Copyright © 2016 Zach Mason. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    
+}
